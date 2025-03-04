862430c011680eff2fe203417541e085a26e739d
Karan Rekhan <159275147+KaranRekhan@users.noreply.github.com>
2025-03-04 23:48:54 +0530

    Added addStudents() method to add a new student to the ArrayList.

--- StudentOperations.java
+++ StudentOperations.java
@@ -1,3 +1,4 @@
+//StuentOperations.java
 import java.util.*;
 class StudentOperations{
 	ArrayList<Student> students; //arraylist created with the class Student as the data type as student has multiple data types
@@ -6,3 +7,7 @@ class StudentOperations{
 		this.students = new ArrayList<>();
 	}
 	// here 'Student' is the datatype, 'students' is the araylist and 'student' is the new entry.
+	public void addStudents(Student student){
+		students.add(student);
+		System.out.println("Student added.");
+	}
