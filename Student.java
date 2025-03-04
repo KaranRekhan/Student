53e89bcf806f9868f846b41c4680a3f10307d620
Karan Rekhan <159275147+KaranRekhan@users.noreply.github.com>
2025-03-04 23:38:06 +0530

    importing libraries

--- Student.java
+++ Student.java
@@ -1,9 +1,12 @@
+import java.util.* ;
+
 class Student{
     private String name;
     private long prn;
     private double gpa;
     private String batch;
     private String branch;
+
     public Student(String name , long prn,double gpa , String batch , String branch){
         this.name = name;
         this.prn = prn;
