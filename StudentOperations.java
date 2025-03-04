2c42c9f680d6f1e5bee7a2d362949ea158a68b89
Karan Rekhan <159275147+KaranRekhan@users.noreply.github.com>
2025-03-04 23:48:13 +0530

    Initial commit: Created StudentOperations class with ArrayList for student management.

---
+++ StudentOperations.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class StudentOperations{
+	ArrayList<Student> students; //arraylist created with the class Student as the data type as student has multiple data types
+	
+	public StudentOperations(){
+		this.students = new ArrayList<>();
+	}
+	// here 'Student' is the datatype, 'students' is the araylist and 'student' is the new entry.
